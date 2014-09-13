024582ff1fae2c2cc286400c11c6fa4344b47803
joaquinPega <joaquin_pega@hotmail.com>
2014-09-13 20:08:58 -0300

    Added hash 256

--- src/test/java/test/mongodb/model/TestMongo.java
+++ src/test/java/test/mongodb/model/TestMongo.java
@@ -1,5 +1,7 @@
 package test.mongodb.model;
 
+import java.security.MessageDigest;
+
 import org.mongodb.model.User;
 
 import com.mongodb.BasicDBObject;
@@ -13,20 +15,27 @@ public class TestMongo {
 	public static void main(String[] args) {
 		try {
 			MongoClient mongoClient = new MongoClient("localhost");
-					
+			User user = new User("joaquin","joaquin_pega@hotmail.com","pass",33654296);
 			DB db = mongoClient.getDB("test"); // test es la base de datos de pruebas
-			
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			String text = "asd";
+
+			md.update(text.getBytes()); 
+			byte[] digest= md.digest();
+			StringBuffer sb = new StringBuffer();
+		    for (int i = 0; i < digest.length; i++) {
+		    	sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+		    }
+			System.out.println(sb.toString());
+			user.setPassword(sb.toString());
 			boolean auth = db.authenticate("test", "test".toCharArray());
 			if (auth) {
 				DBCollection coll = db.getCollection("users"); // Nombre de la coleccion
-				DBCursor cursor= coll.find(new BasicDBObject("name","joaquin"));
-				while(cursor.hasNext()){
-					//System.out.println(cursor.next());
-					BasicDBObject obj = (BasicDBObject)cursor.next();
-					User j= new User(obj.getString("name"),obj.getString("email"),obj.getString("password"),obj.getInt("dni"));
-					System.out.println(j.getNombre());
-				}
-				System.out.println();
+				BasicDBObject doc = new BasicDBObject("name",user.getNombre())
+				.append("password", user.getPassword())
+				.append("email", user.getMail())
+				.append("dni", user.getDni());
+				coll.insert(doc);
 			} else {
 				System.out.println("Fallo la autentificacion");			//Fallo la autentificacion igual esta deprecated, habria que mirar otro
 			}
